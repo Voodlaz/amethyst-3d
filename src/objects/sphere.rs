2c15e3b4f2f3bb217f786729b236b16509b42e82
Voodlaz <47495939+Voodlaz@users.noreply.github.com>
2020-06-18 18:55:33 +0300

    n

--- src/objects/sphere.rs
+++ src/objects/sphere.rs
@@ -1,10 +1,22 @@
-use amethyst::prelude::*;
-
-use amethyst::assets::AssetLoaderSystemData;
-
-fn init_sphere(world: &mut World) {
-
-    world.create_entity()
-        .with(/*???*/)
-        .build();
-}
+use amethyst::{
+    assets::{Format as AssetFormat, Handle, Loader},
+    core::{math::Vector3, Transform, TransformBundle},
+    ecs::{World, WorldExt},
+    error::Error,
+    input::{InputBundle, StringBindings},
+    prelude::*,
+    renderer::{
+        camera::Camera,
+        light::{Light, PointLight},
+        mtl::{Material, MaterialDefaults},
+        palette::{Srgb, Srgba},
+        plugins::{RenderShaded3D, RenderSkybox, RenderToWindow},
+        rendy::{
+            mesh::{MeshBuilder, Normal, Position, TexCoord},
+            texture::palette::load_from_srgba,
+        },
+        types::{DefaultBackend, Mesh, MeshData},
+        RenderingBundle,
+    },
+    utils::application_root_dir,
+};
