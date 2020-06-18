a879a85772c4e20d21b6e323f94682e9afec0947
Voodlaz <47495939+Voodlaz@users.noreply.github.com>
2020-06-18 12:09:53 +0300

    h

---
+++ src/objects/sphere.rs
@@ -0,0 +1,10 @@
+use amethyst::prelude::*;
+
+use amethyst::assets::AssetLoaderSystemData;
+
+fn init_sphere(world: &mut World) {
+
+    world.create_entity()
+        .with(/*???*/)
+        .build();
+}
