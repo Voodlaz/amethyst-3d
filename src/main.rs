a9e1d47186fbfd7b434cebff4ea83df7f8d005ed
Voodlaz <47495939+Voodlaz@users.noreply.github.com>
2020-06-19 15:23:33 +0300

    k

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,10 @@ use amethyst::{
     utils::application_root_dir,
 };
 
+use std::fs::File;
+use std::io::BufReader;
+use obj::{load_obj, Obj};
+
 mod objects;
 use crate::objects::{camera, sphere};
 
@@ -29,87 +33,26 @@ struct MyState;
 #[derive(Clone, Debug)]
 struct Custom;
 
-impl AssetFormat<MeshData> for Custom {
+impl AssetFormat for Custom {
     fn name(&self) -> &'static str {
         "CUSTOM"
     }
 
-    /// Reads the given bytes and produces asset data.
-    fn import_simple(&self, bytes: Vec<u8>) -> Result<MeshData, Error> {
-        let data: String = String::from_utf8(bytes)?;
-        let trimmed: Vec<&str> = data.lines().filter(|line| !line.is_empty()).collect();
+    fn import_simple() -> Result<MeshData, Error> {
+        let sphere_obj = BufReader::new(File::open("assets/sphere_mesh.obj")?);
+        let sphere: Obj = load_obj(sphere_obj)?;
 
-        let mut pos = Vec::with_capacity(trimmed.len() * 3);
-        let mut norm = Vec::with_capacity(trimmed.len() * 3);
-        let mut tex = Vec::with_capacity(trimmed.len() * 3);
-
-        for line in trimmed {
-            let nums: Vec<&str> = line.split_whitespace().collect();
-            pos.push(Position([
-                nums[0].parse::<f32>().unwrap(),
-                nums[1].parse::<f32>().unwrap(),
-                nums[2].parse::<f32>().unwrap(),
-            ]));
-            norm.push(Normal([
-                nums[3].parse::<f32>().unwrap(),
-                nums[4].parse::<f32>().unwrap(),
-                nums[5].parse::<f32>().unwrap(),
-            ]));
-            tex.push(TexCoord([0.0, 0.0]))
-        }
         Ok(MeshBuilder::new()
-            .with_vertices(pos)
-            .with_vertices(norm)
-            .with_vertices(tex)
+            .with_vertices(sphere.vertices)
             .into())
     }
 }
 
 impl SimpleState for MyState {
     fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
-        let StateData { world, .. } = data;
-        world.insert(0usize);
-
         camera::init_camera(world);
-        //init_lights(world);
 
-        // Add custom cube object to scene
-        let (mesh, mtl) = {
-            let mat_defaults = world.read_resource::<MaterialDefaults>();
-            let loader = world.read_resource::<Loader>();
-
-            let meshes = &world.read_resource();
-            let textures = &world.read_resource();
-            let materials = &world.read_resource();
-
-            let mesh: Handle<Mesh> = loader.load("sphere_mesh.obj", Custom, (), meshes);
-            let albedo = loader.load_from_data(
-                load_from_srgba(Srgba::new(0.1, 0.5, 0.3, 1.0)).into(),
-                (),
-                textures,
-            );
-            let mat: Handle<Material> = loader.load_from_data(
-                Material {
-                    albedo,
-                    ..mat_defaults.0.clone()
-                },
-                (),
-                materials,
-            );
-
-            (mesh, mat)
-        };
-
-        let mut trans = Transform::default();
-        trans.set_translation_xyz(-5.0, 0.0, 0.0);
-        trans.set_scale(Vector3::new(2.0, 2.0, 2.0));
-        world
-            .create_entity()
-            .with(mesh)
-            .with(mtl)
-            .with(trans)
-            .build();
-    }
+        // what I should put here
 }
 
 fn main() -> amethyst::Result<()> {
