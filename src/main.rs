728e40c53bac226c0b5bef510b600ecdf390a879
Voodlaz <47495939+Voodlaz@users.noreply.github.com>
2020-06-20 00:50:05 +0300

    h

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,7 @@ use amethyst::{
     utils::application_root_dir,
 };
 use std::io::Cursor;
+use obj::{load_obj, Obj};
 
 mod objects;
 use crate::objects::{camera, sphere};
@@ -67,11 +68,48 @@ impl AssetFormat<MeshData> for ObjMesh {
 
 impl SimpleState for MyState {
     fn on_start(&mut self,data: StateData<'_, GameData<'_, '_>>) {
-        let world = data.world;
+        let StateData { world, .. } = data;
+        world.insert(0usize);
 
         camera::init_camera(world);
 
-        // how I can load this
+        // Add custom cube object to scene
+        let (mesh, mtl) = {
+            let mat_defaults = world.read_resource::<MaterialDefaults>();
+            let loader = world.read_resource::<Loader>();
+
+            let meshes = &world.read_resource();
+            let textures = &world.read_resource();
+            let materials = &world.read_resource();
+
+            let mesh: Handle<Mesh> = loader.load("sphere_mesh.obj", ObjMesh, (), meshes);
+            let albedo = loader.load_from_data(
+                load_from_srgba(Srgba::new(0.1, 0.5, 0.3, 1.0)).into(),
+                (),
+                textures,
+            );
+            let mat: Handle<Material> = loader.load_from_data(
+                Material {
+                    albedo,
+                    ..mat_defaults.0.clone()
+                },
+                (),
+                materials,
+            );
+
+            (mesh, mat)
+        };
+
+        let mut trans = Transform::default();
+        trans.set_translation_xyz(-5.0, 0.0, 0.0);
+        trans.set_scale(Vector3::new(2.0, 2.0, 2.0));
+        world
+            .create_entity()
+            .with(mesh)
+            .with(mtl)
+            .with(trans)
+            .build();
+    }
     }
 
 fn main() -> amethyst::Result<()> {
