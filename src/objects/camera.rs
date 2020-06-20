4e908c66f4958d1e7ff8c13057e58e256212d04f
Voodlaz <47495939+Voodlaz@users.noreply.github.com>
2020-06-20 16:33:04 +0300

    j

--- src/objects/camera.rs
+++ src/objects/camera.rs
@@ -7,7 +7,7 @@ use amethyst::core::Transform;
 pub fn init_camera(world: &mut World) {
     let mut transform = Transform::default();
     transform.set_translation_xyz(0.0, -20.0, 10.0);
-    transform.prepend_rotation_x_axis(1.325);
+    transform.prepend_rotation_x_axis(1.325_752_1);
 
     let (width, height) = {
     let dim = world.read_resource::<ScreenDimensions>();
