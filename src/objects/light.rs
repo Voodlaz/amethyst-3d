2b21e5d131a144cbf8602b2d897558807c15701d
Voodlaz <47495939+Voodlaz@users.noreply.github.com>
2020-06-21 18:45:24 +0300

    k

--- src/objects/light.rs
+++ src/objects/light.rs
@@ -8,14 +8,14 @@ use amethyst::prelude::*;
 pub fn init_light(world: &mut World) {
     let light: Light = PointLight {
         intensity: 100.0,
-        radius: 50.0,
+        radius: 1.0,
         color: Srgb::new(1.0, 1.0, 1.0),
         ..Default::default()
     }
     .into();
 
     let mut transform = Transform::default();
-    transform.set_translation_xyz(5.0, 5.0, 20.0);
+    transform.set_translation_xyz(0.0, 0.0, 0.0);
 
     // Add point light.
     world.create_entity().with(light).with(transform).build();
